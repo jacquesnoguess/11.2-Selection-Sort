6d166c7df1f5d7cae00abbb557242b85c551c9cd
j <j>
2026-03-06 09:30:44 -0600

    ghjk

--- Main.java
+++ Main.java
@@ -18,6 +18,17 @@ public class Main {
     }
 
     public static void selectionSort(int[] array) {
-        
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap the found minimum element with the current element
+            int temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
+        }
     }
 }
